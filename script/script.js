ba68a3e44f7c505ee237d6ad9f30fc500730b2b6
Alexander <aleck_johansson@hotmail.com>
2024-10-30 19:01:05 +0100

    Variabler och HTML-element

---
+++ script/script.js
@@ -0,0 +1,17 @@
+//Variabler
+
+const words = ["javascript", "programmering", "kodning", "dator", "utveckling"] //Skapar Array med ord
+let selectedWord = words[Math.floor(Math.random() * words.length)] //Hämtar ett random ord från Array
+let guessedLetters = [] //Tom Array för att spara bokstäver man gissat på
+let incorrectLetters = [] //Tom Array för att spara felaktiga bokstäver
+let maxErrors = 6
+let errors = 0
+
+//Hämtar html-element
+const wordDisplay = document.getElementById("word-display")
+const incorrectLettersDisplay = document.getElementById("incorrect-letters")
+const guessInput = document.getElementById("guess-input")
+const guessBtn = document.getElementById("guess-btn")
+
+
+
