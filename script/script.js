7783227925f876767df6098b8d683174c7791b2f
Karlgit13 <karl.vareskog@gmail.com>
2024-10-31 08:42:00 +0100

    SVG & resetGame

--- script/script.js
+++ script/script.js
@@ -14,11 +14,11 @@ const guessInput = document.getElementById("guess-input")
 const guessBtn = document.getElementById("guess-btn")
 
 
-    //updaterar visning av felaktiga bokstäver
+//updaterar visning av felaktiga bokstäver
 const updateIncorrectLettersDisplay = () => {
     incorrectLettersDisplay.textContent = `Felaktiga bokstäver: ${incorrectLetters.join(", ")}`
 }
-    // Kontrollerar om spelaren vunnit eller förlorat
+// Kontrollerar om spelaren vunnit eller förlorat
 const checkGameStatus = () => {
     if (selectedWord.split("").every(letter => guessedLetters.includes(letter))) {
         alert("grattis du vann!")
@@ -29,3 +29,30 @@ const checkGameStatus = () => {
         console.log(checkGameStatus)
     }
 }
+
+// uppdaterar hangman SVG-bilden beroende på fel
+function updateHangmanImage() {
+    const parts = ["head", "body", "arms", "legs"];
+    if (errors > 0 && errors <= maxErrors) {
+        document.getElementById(parts[errors - 1]).style.display = "block";
+    }
+}
+
+
+// nollställer spelet
+const resetGame = () => {
+    selectedWord = words[Math.floor(Math.random() * words.length)]
+    guessedLetters = []
+    incorrectLetters = []
+    errors = 0
+    updateWordDisplay()
+    updateIncorrectLettersDisplay()
+    document.querySelectorAll("path, ellipse").forEach(part => (part.style.display = "none"))
+    document.getElementById("ground").style.display = "block";
+}
+
+
+
+guessBtn.addEventListener("click", handleGuess)
+updateWordDisplay()
+resetGame()
